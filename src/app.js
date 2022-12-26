c1c0fde1a921dcc39a63c5af5c3eb409a3a88712
BAbdalwahed <b.abdalwahed@hotmail.com>
2022-12-26 23:51:32 +0000

    first test passed

--- src/app.js
+++ src/app.js
@@ -1,14 +1,3 @@
-/*const express = require('express');
-const artistRouter = require('./routes/artist');
-
-const app = express();
-
-app.use(express.json());
-
-app.use('/artist', artistRouter);
-
-module.exports = app;*/
-
 const express = require('express');
 const artistRouter = require('../routes/artist');
 
